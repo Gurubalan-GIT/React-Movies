5b2134b8dc688b189637fff47a3d232366bb5cca
Gurubalan-GIT <gurubalan.work@gmail.com>
2020-04-26 01:55:52 +0530

    fix: container class names in production

--- src/components/Actors/Movie.js
+++ src/components/Actors/Movie.js
@@ -14,19 +14,15 @@ function Movie(props) {
     return (
         <div className="cast-div card card-body bg-light">
             <Col>
-                <div>
-                    <Col>
-                        {   // NOTE The AWS link is actually a No Image found ( image ), this will come in place if there is not image available in the API
-                            <Image alt="movie-img found" src={`https://image.tmdb.org/t/p/w185${props.movie.poster_path}`} roundedCircle className="cast-img"/>
-                        }
-                    </Col>
+                {   // NOTE The AWS link is actually a No Image found ( image ), this will come in place if there is not image available in the API
+                    <Image alt="movie-img found" src={`https://image.tmdb.org/t/p/w185${props.movie.poster_path}`} roundedCircle className="cast-img"/>
+                }
+            </Col>
 
-                    <Col> 
-                        <h5>{props.movie.title}</h5> 
-                        <p>{props.movie.character}</p>
-                        <Link to={`/movies/${props.id}`}><Button variant="info" size="sm">Know more</Button></Link>
-                    </Col>
-                </div>
+            <Col> 
+                <h5>{props.movie.title}</h5> 
+                <p>{props.movie.character}</p>
+                <Link to={`/movies/${props.id}`}><Button variant="info" size="sm">Know more</Button></Link>
             </Col>
         </div>
     );
--- src/components/Movies/Cast.js
+++ src/components/Movies/Cast.js
@@ -13,21 +13,19 @@ import '../../styles/component.css';
 function Cast(props) {
     return (
         <div className="cast-div card card-body bg-light">
+        
             <Col>
-                <div>
-                    <Col>
-                        {   // NOTE The AWS link is actually a No Image found ( image ), this will come in place if there is not image available in the API
-                            <Image alt="movie-img found" src={`https://image.tmdb.org/t/p/w185${props.cast.profile_path}`} roundedCircle className="cast-img"/>
-                        }
-                    </Col>
-
-                    <Col> 
-                        <h5>{props.cast.name}</h5> 
-                        <p>{props.cast.character}</p>
-                        <Link to={`/actors/${props.id}`}><Button variant="info" size="sm">Know more</Button></Link>
-                    </Col>
-                </div>
+                {   // NOTE The AWS link is actually a No Image found ( image ), this will come in place if there is not image available in the API
+                    <Image alt="movie-img found" src={`https://image.tmdb.org/t/p/w185${props.cast.profile_path}`} roundedCircle className="cast-img"/>
+                }
             </Col>
+
+            <Col> 
+                <h5>{props.cast.name}</h5> 
+                <p>{props.cast.character}</p>
+                <Link to={`/actors/${props.id}`}><Button variant="info" size="sm">Know more</Button></Link>
+            </Col>
+    
         </div>
     );
 }
