f46e78680274554248a237ddf4ada0803da25bd5
Gurubalan-GIT <gurubalan.work@gmail.com>
2020-04-26 00:05:15 +0530

    feat: updated jumbotron for home component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import NavBar from './components/NavBar.js';
 import Movies from './components/Movies/Movies.js';
 import Actors from './components/Actors/Actors.js';
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import MovieDetails from './components/Movies/MovieDetails.js'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import MovieDetails from './components/Movies/MovieDetails.js';
+import ActorDetails from './components/Actors/ActorDetails.js';
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/" exact component={HomePage} />
-          <Route path="/actors" component={Actors} />
+          <Route path="/actors" exact component={Actors} />
+          <Route path="/actors/:id" component={ActorDetails} />
           <Route path="/movies" exact component={Movies} />
-          <Route path="/movies/:id" component={MovieDetails}/>
+          <Route path="/movies/:id" component={MovieDetails} />
         </Switch>
       </div>
     </Router>
@@ -24,7 +26,15 @@ function App() {
 
 const HomePage = () => (
   <div>
-    <h2>Home page!</h2>
+    <br></br>
+    <br></br>
+    <br></br>
+    <div className="jumbotron text-center">
+      <h1 className="display-4">Movie DB Application</h1>
+      <p className="lead">An organised and efficient application for Movies using TMDB API.</p>
+      <hr className="my-4"></hr>
+      <p>Check it out!</p>
+    </div>
   </div>
 );
 
