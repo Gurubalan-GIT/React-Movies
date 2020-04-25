f40bac2288ddbe4c994067b4c301820a643535b6
Gurubalan-GIT <gurubalan.work@gmail.com>
2020-04-26 02:03:09 +0530

    fix: react bootstrap conflict

--- src/components/Actors/Movie.js
+++ src/components/Actors/Movie.js
@@ -13,17 +13,17 @@ import '../../styles/component.css';
 function Movie(props) {
     return (
         <div className="cast-div card card-body bg-light">
-            <Col>
+            <div className="col">
                 {   // NOTE The AWS link is actually a No Image found ( image ), this will come in place if there is not image available in the API
-                    <Image alt="movie-img found" src={`https://image.tmdb.org/t/p/w185${props.movie.poster_path}`} roundedCircle className="cast-img"/>
+                    <Image className="cast-img" alt="movie-img found" src={`https://image.tmdb.org/t/p/w185${props.movie.poster_path}`} roundedCircle />
                 }
-            </Col>
+            </div>
 
-            <Col> 
+            <div className="col"> 
                 <h5>{props.movie.title}</h5> 
                 <p>{props.movie.character}</p>
                 <Link to={`/movies/${props.id}`}><Button variant="info" size="sm">Know more</Button></Link>
-            </Col>
+            </div>
         </div>
     );
 }
